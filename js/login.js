08e0c3bc93d1fd5a46f889edd4c2395186e935e0
shonson-hub <shonborland@gmail.com>
2020-11-11 22:44:32 +0300

    мастер-ремастер защита

--- js/login.js
+++ js/login.js
@@ -3,11 +3,7 @@ const popup = document.querySelector(".modal-login");
 const close = document.querySelector(".modal-close");
 const login = document.querySelector(".login-user");
 const form = popup.querySelector("form");
-const slider = document.querySelector(".features");
-const slideFst = document.querySelector(".slider-button-fst");
-const slideSnd = document.querySelector(".slider-button-snd");
-const slideThd = document.querySelector(".slider-button-trd");
-const cleardot = document.querySelectorAll(".current");
+
 
 contact.addEventListener("click",function (evt) {
   evt.preventDefault();
